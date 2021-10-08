d1c35926df0b54d35ebdf1d54ed65038522abf83
Ramon Mora Marquez <60113219+rmora-marquez@users.noreply.github.com>
2021-10-08 18:38:54 -0500

    agregado interfaz y boton

--- app/src/main/java/edu/ieu/miprimeraapp/MainActivity.java
+++ app/src/main/java/edu/ieu/miprimeraapp/MainActivity.java
@@ -3,6 +3,7 @@ package edu.ieu.miprimeraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**/
+    public void sendMessage(View view){
+        System.out.println("hice click en el botón");
     }
 }
